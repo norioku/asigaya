af538518c53d593740e7f272ab180e2988a3927b
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2019-12-18 07:30:39 +0000

    test

--- db/migrate/20191208075446_add_image_to_users.rb
+++ db/migrate/20191208075446_add_image_to_users.rb
@@ -2,4 +2,17 @@ class AddImageToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :image, :string
   end
+  
+  def change
+    add_column :users, :admin, :boolean, default: false
+  end
+  
+  def change
+    add_column :users, :profile, :text
+  end
+  
+  def change
+    remove_column :users, :image, :string
+  end
+  
 end
