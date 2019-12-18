586589a752ab99f0d0a83f32e7418fdff67fece9
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2019-12-18 07:31:09 +0000

    Revert "test"
    This reverts commit af538518c53d593740e7f272ab180e2988a3927b.

--- db/migrate/20191208075446_add_image_to_users.rb
+++ db/migrate/20191208075446_add_image_to_users.rb
@@ -2,17 +2,4 @@ class AddImageToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :image, :string
   end
-  
-  def change
-    add_column :users, :admin, :boolean, default: false
-  end
-  
-  def change
-    add_column :users, :profile, :text
-  end
-  
-  def change
-    remove_column :users, :image, :string
-  end
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2019_12_14_054806) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
     t.string "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
