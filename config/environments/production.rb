f822a29344b9b5108d7b572c46b1effe44da1fa6
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2019-12-24 06:00:02 +0000

    add domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://rds-mysql-server-jacketguy.cofslnulaq62.ap-northeast-1.rds.amazonaws.com/', /http:\/\/rds-mysql-server-jacketguy.cofslnulaq62.ap-northeast-1.rds.amazonaws.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://asigaya.tokyo/', /http:\/\/asigaya.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
